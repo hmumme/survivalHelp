3786705bf25a93911551a1fcfc830ed782e25e1e
Hope Mumme <hmumme@pmedseq.bmi.emory.edu>
2021-12-17 10:25:37 -0500

    mSet

--- R/functions.R
+++ R/functions.R
@@ -10,13 +10,13 @@
 #' provide cutoff threshold and gene expression values for specific gene
 #'
 #' @param gene the gene we want to use to classify samples based on expression
-#' @param mat tibble with samples and gene expression (FPKM)
+#' @param mat expression values (FPKM) tibble with samples as columns and symbol column containing gene names
 #' @param os tibble with samples, overall survival (OS) values, and Vital.Status values
 #' @param cut method to use to separate into high and low groups, can be either median (defualt) or cutP
 #' @return the cutoff threshold and expression values (log2(FPKM+1)) as a list
 #' @export
 #' @examples
-#' data = data.frame(data = matrix(rnorm(100),ncol=10))
+#' data = abs(data.frame(data = matrix(rnorm(100),ncol=10)))
 #' expr = dplyr::tibble(symbol = paste0(rep("gene",10),1:10), data)
 #' colnames(expr) = c("symbol", letters[1:10])
 #' os = dplyr::tibble(sample = letters[1:10], OS = 10:19, Vital.Status = c(rep("Alive",8),rep("Dead",2)))
@@ -44,48 +44,46 @@ mCut = function(gene, mat, os, cut = "median") {
     return(out)
 }
 
-# function mSet 
-    # inputs:
-    # gs -- gene set we want to use to classify samples based on median expression
-    # mat -- matrix with rownames as Ensembl IDs and colnames as sample names
-    # os -- tibble with samples, OS values, empty column "group"
-    # ref -- reference tibble to use with helper function
-    # cut -- method to use to separate into high and low (either median (default) or cutP)
-    # outputs:
-    # os -- cutoff value and GSVA scores in list
-mSet = function(gs, mat, os, ref, cut = "median") {
+#' provide cutoff threshold and GSEA values for gene set of two or more genes
+#'
+#' @param gs the genes we want to use to classify samples based on expression
+#' @param ENS expression values (FPKM) tibble with samples as columns and ENSMBL ids as rows
+#' @param os tibble with samples, overall survival (OS) values, and Vital.Status values
+#' @param ref reference tibble with ensmble ids and gene symbols as columns, converts to ENS using this
+#' @param cut method to use to separate into high and low groups, can be either median (defualt) or cutP
+#' @return the cutoff threshold and GSEA scores as a list
+#' @export
+#' @examples
+#' expr = abs(matrix(rnorm(100),ncol=10))
+#' rownames(expr) = paste0(rep("ENSG",10),1:10)
+#' colnames(expr) = letters[1:10]
+#' ref = dplyr::tibble(ensg = paste0(rep("ENSG",10), 1:10), symbol = paste0(rep("gene",10), 10:1))
+#' os = dplyr::tibble(sample = letters[1:10], OS = 10:19, Vital.Status = c(rep("Alive",8),rep("Dead",2)))
+#' mSet(c("gene1","gene4","gene5"), ENS = expr, os = os, ref = ref, cut = "cutP")
+mSet = function(gs, ENS, os, ref, cut = "median") {
     gs = findENS(gs, ref) # convert to ensmbl ids
-    mat = mat[rownames(mat) %in% unlist(gs),] # extract only genes of interest
-    mat = log2(mat + 1) # retrieve log2(FPKM+1)
-    scores = callGSVA(mat,gs)
-    #scores = suppressWarnings(gsva(mat, gs, verbose=FALSE, method="zscore", parallel.sz=2)) # calculate ssGSEA enrichment scores
+    ENS = ENS[rownames(ENS) %in% unlist(gs),] # extract only genes of interest
+    ENS = log2(ENS + 1) # retrieve log2(FPKM+1)
+    scores = callGSVA(ENS,gs)
     if (cut == "median") {
         cutoff = median(scores) # find median score for gene set
-        #highSamples = colnames(mat)[scores > med]
     } else if (cut == "cutP") {
         os["Scores"] = 0
         # add scores into os
         for (i in 1:length(scores)) {
-            #os[os$sample == colnames(scores)[i],"Scores"] = scores[i]
             os[os$sample == rownames(scores)[i],"Scores"] = scores[i]
         }
-        cox.os = coxph(Surv(time=os$OS, event=os$Vital.Status=="Dead")~Scores, data = os) # get survival based on GSEA scores
-        c = cutp(cox.os)$Scores # get cutp output
+        cox.os = suppressWarnings(survival::coxph(survival::Surv(time=os$OS, event=os$Vital.Status=="Dead")~Scores, data = os)) # get survival based on GSEA scores
+        c = survMisc::cutp(cox.os)$Scores # get cutp output
         data.table::setorder(c, Scores) # create table with cut points
         percentile <- ecdf(c$Scores) # find cdf of GSEA score points
         cutoff <- as.numeric(c[order(c$Q), ][nrow(c), 1]) # find optimal GSEA score CutOff
-        #highSamples = colnames(mat[,-1])[scores > signif(low,3)] # divide samples
-        #lowSamples = colnames(mat[,-1])[scores <= signif(low,3)] # divide samples
         print(paste0("Cutoff = ", signif(cutoff,3)))
     } else {
         print("invalid cut method, please insert either median or cutP ... returning os without separation")
         os$group = "Low"
         return(os)
     }
-    #os$group[os$sample %in% lowSamples] = "Low"
-    #os$group[os$sample %in% highSamples] = "High"
-    #os$group = factor(os$group, levels = c("Low","High"))# change levels so that the reference group is Low expression
-    #return(os)
     out = list(signif(cutoff,3),scores)
     names(out) = c("cutoff","scores")
     return(out) # return cutoff point and Scores
@@ -113,34 +111,6 @@ findENS = function(genes, ref) {
     return(ens)
 }
 
-# function plotSurv -- not working
-    # inputs: 
-    # gene -- gene we want to use to split expression, and then calculate survival from
-    # mat -- tibble with samples and gene expression
-    # os -- tibble with samples, OS values, empty column "group"
-    # subtype -- type of MPAL we are analyzing (BM or TM)
-    # outputs: 
-    # save plot in plots folder - GENENAMEsurvivalSUBTYPE.pdf
-    # if gene not found, print "gene not found in dataset"
-plotSurv = function(gene, mat, ENS, os, subtype, subfolder, ref) {
-    if (length(gene) > 1) {
-        os = suppressWarnings(mSet(gene, ENS, os, ref)) # calculate median, classify as high/low expression
-    } else {
-        os = suppressWarnings(mCut(gene, mat, os)) # calculate median, classify as high/low expression
-    }
-    # check if gene is not found in dataset
-    if (length(unique(os$group)) == 1) {
-        print("Gene not found in dataset!")
-    } else {
-        pdf(file = paste0("plots/", subfolder, "/",gene,"survival",subtype,".pdf"))
-        plot(survfit(Surv(time=os$OS, event=os$Vital.Status=="Dead")~group, data = os), 
-            main = paste0("Plot of Survival Curves by ",gene," Expression in ",subtype," MPAL"), 
-            xlab = "Length of Survival (days)",ylab="Probability of Survival",col=c("red","blue"))
-        legend("topright", legend=c("High","Low"),fill=c("red","blue"),bty="n")
-        dev.off()
-    }
-}
-
 # function survStats
     # inputs:
     # os -- tibble with samples, OS values, and filled in column "group"
@@ -157,23 +127,6 @@ survStats = function(os) {
     return(stats)
 }
 
-# function checkSurv
-checkSurv = function(gene, ENS, mat, os, ref) {
-    if (length(gene) > 1) {
-        os = mSet(gs, ENS, os, ref)
-    } else {
-        os = mCut(gene, mat, os)
-    }
-    
-    check = data.frame("OS" = os$OS, "group" = os$group, "geneExp" = 0)
-    for (i in 1:nrow(check)) {
-        sample_i = os$sample[i] # extract sample id from os
-        check$geneExp[i] = unlist(mat[mat$symbol == gene, colnames(mat) == sample_i]) # add gene exp for gene/sample into check
-    }
-    print(check %>% group_by(group) %>% summarize(meanExp = mean(geneExp), meanOS = mean(OS)))
-
-    return(check)
-}
 
 # FROM SURVIVALGENIE
 #'@name callGSVA
